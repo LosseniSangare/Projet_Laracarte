6acafebadd6ad42b88dd45241ac7f47262432c1e
LosseniSangare <lsangare2020@gmail.com>
2019-11-27 16:29:15 +0000

    retour modification register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,25 +5,55 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="text-center card-header">{{ __('Enrégistrement') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="nom" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="nom" type="text" class="form-control @error('nom') is-invalid @enderror" name="nom" value="{{ old('nom') }}" required autocomplete="nom" autofocus>
 
-                                @error('name')
+                                @error('nom')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
+
+
+                        <div class="form-group row">
+                            <label for="penoms" class="col-md-4 col-form-label text-md-right">{{ __('Prenoms') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="penoms" type="text" class="form-control @error('penoms') is-invalid @enderror" name="penoms" value="{{ old('penoms') }}" required autocomplete="penoms" autofocus>
+
+                                @error('penoms')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="utilisateur" class="col-md-4 col-form-label text-md-right">{{ __('Nom d\'tilisateur') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="utilisateur" type="text" class="form-control @error('utilisateur') is-invalid @enderror" name="utilisateur" value="{{ old('utilisateur') }}" required autocomplete="utilisateur" autofocus>
+
+                                @error('utilisateur')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
